69843d91020f11b71706d7cef0f0eb446c150dc4
elhan <elhan.asanov@gmail.com>
2020-11-23 17:14:09 +0600

    add homework

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,13 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello!");
+        Ball ballOne = new Ball();
+
+        ballOne.color = "BLUE";
+        ballOne.price = 20;
+        ballOne.radius = 6;
+        ballOne.producerCountry = "GERMANY";
+
+        System.out.println(ballOne.color + " " + ballOne.price + " " + ballOne.producerCountry);
     }
 }
